f66fc214982e95310cb5689c2a4600703a7e0676
Евгений <@EUGENE1984>
2023-11-11 12:04:46 +0500

    Добавьте файлы проекта.

---
+++ Device/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+class Device
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public Device(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public virtual void Sound()
+    {
+        Console.WriteLine($"{Name} издает звук.");
+    }
+
+    public void Desc()
+    {
+        Console.WriteLine($"Описание: {Description}");
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
+class Kettle : Device
+{
+    public Kettle(string name, string temperatureRange)
+        : base(name, $"Чайник с рабочим интервалом температур: {temperatureRange}")
+    {
+    }
+
+    public override void Sound()
+    {
+        Console.WriteLine($"{Name} бурлит.");
+    }
+}
+
+class Microwave : Device
+{
+    public Microwave(string name, string power)
+        : base(name, $"Микроволновка мощьностью: {power}")
+    {
+    }
+
+    public override void Sound()
+    {
+        Console.WriteLine($"{Name} гудит.");
+    }
+}
+
+class Car : Device
+{
+    public Car(string name, string model)
+        : base(name, $"Форма автомобиля: {model}")
+    {
+    }
+
+    public override void Sound()
+    {
+        Console.WriteLine($"{Name} бибикает.");
+    }
+}
+
+class Ship : Device
+{
+    public Ship(string name, string type)
+        : base(name, $"Тип корабля: {type}")
+    {
+    }
+
+    public override void Sound()
+    {
+        Console.WriteLine($"{Name} гудит.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Kettle kettle = new Kettle("Электрочайник", "50°C - 100°C");
+        Microwave microwave = new Microwave("Микроволновка", "800W");
+        Car car = new Car("Автомобиль", "Седан");
+        Ship ship = new Ship("Корабль", "Грузовой");
+
+        Device[] devices = { kettle, microwave, car, ship };
+
+        foreach (Device device in devices)
+        {
+            Console.WriteLine($"Устройство: {device}");
+            device.Sound();
+            device.Desc();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Human/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+public abstract class Human
+{
+    protected string firstName;
+    protected string lastName;
+    protected DateTime birthDate;
+
+    public Human()
+    { }
+
+    public Human(string fName, string lName)
+    {
+        firstName = fName;
+        lastName = lName;
+    }
+
+    public Human(string fName, string lName, DateTime date) : this(fName, lName)
+    {
+        birthDate = date;
+    }
+
+    public override string ToString()
+    {
+        return $"Фамилия: {lastName}\n" +
+            $"Имя: {firstName}\n" +
+            $"Дата рождения: {birthDate.ToShortDateString()}\n";
+    }
+}
+
+public abstract class Employee : Human
+{
+    double _salary;
+
+    public Employee(string fName, string lName) : base(fName, lName) { }
+
+    public Employee(string fName, string lName, double salary) : base(fName, lName)
+    {
+        _salary = salary;
+    }
+
+    public Employee(string fName, string lName, DateTime date, double salary)
+        : base(fName, lName, date)
+    {
+        _salary = salary;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $"Зарплата: {_salary} руб.\n";
+    }
+}
+public class Builder : Employee
+{
+    string specialty;
+
+    public Builder(string fName, string lName, double salary, string specialty, DateTime date)
+        : base(fName, lName, date, salary)
+    {
+        this.specialty = specialty;
+    }
+
+    public void Build(string buildingName)
+    {
+        Console.WriteLine($"Строитель {firstName} {lastName}  по специальности {specialty} укладывает кирпичи в {buildingName}.");
+    }
+}
+
+public class Sailor : Employee
+{
+    string shipName;
+
+    public Sailor(string fName, string lName, double salary, string shipName, DateTime date)
+        : base(fName, lName, date, salary)
+    {
+        this.shipName = shipName;
+    }
+
+    public void Sail()
+    {
+        Console.WriteLine($"Капитан {firstName} {lastName} управляет кораблем '{shipName}'.");
+    }
+}
+
+public class Pilot : Employee
+{
+    string aircraftName;
+
+    public Pilot(string fName, string lName, double salary, string aircraftName, DateTime date)
+        : base(fName, lName, date, salary)
+    {
+        this.aircraftName = aircraftName;
+    }
+
+    public void Fly()
+    {
+        Console.WriteLine($"Пилот {firstName} {lastName} управляет самолетом '{aircraftName}'.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Builder builder = new Builder("Вася", "Пупкин", 50000, "каменщик", new DateTime(1990, 5, 15));
+        builder.Build("больнице");
+        Console.WriteLine(builder.ToString());
+
+        Sailor sailor = new Sailor("Джек", "Воробей", 45000, "Черная Жемчужина", new DateTime(1985, 10, 20));
+        sailor.Sail();
+        Console.WriteLine(sailor.ToString());
+
+        Pilot pilot = new Pilot("Олег", "Ползунов", 75000, "Боинг 737", new DateTime(1988, 3, 8));
+        pilot.Fly();
+        Console.WriteLine(pilot.ToString());
+    }
+}
